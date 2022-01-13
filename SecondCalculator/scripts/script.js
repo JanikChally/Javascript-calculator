27ea1a75913cfca78927c64c8ad4e5fd0dc00199
Janik Chally <janikchally@gmail.com>
2022-01-12 19:32:43 -0600

    Added functionality to number buttons, added working negative/positive button, added working clear key, next step is to add functionality to operator buttons and equals buttons

--- SecondCalculator/scripts/script.js
+++ SecondCalculator/scripts/script.js
@@ -4,12 +4,40 @@ let buttons = document.querySelector('.calc-button');
 
 buttons.addEventListener('click', (event) => {
     const elmt = event.target;
+    const display = document.querySelector('.calc-input');
     console.log(elmt)
-    
-    if(elmt.id === 'double'){
+
+    if (elmt.id === 'double') {
         document.querySelector('.calc-input').value *= 2;
-    }
-    else if(elmt.id === 'third'){
+
+    } else if (elmt.id === 'third') {
         document.querySelector('.calc-input').value *= .33;
+
+    } else if (elmt.className === 'number-button') {
+        if(display.value === '0'){
+            display.value = elmt.textContent;
+        }else{
+            display.value += elmt.textContent;
+        }
+
+    }else if(elmt.id === 'clear'){
+        display.value = 0;
+    }else if (elmt.id ==='decimal'){
+        if(!display.value.includes('.')){
+            display.value += elmt.textContent;
+        }
+    }else if(elmt.id ==='negative'){
+        if(!display.value.includes('-')){
+            if(display.value !=='0'){
+                display.value = '-' + display.value;
+            }
+        }
+        else {
+            display.value = display.value.slice(1);
+        }
     }
+
+
+
 })
+
