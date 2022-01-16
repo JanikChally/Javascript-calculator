f45762d61eb35af7d59146e46fccfc6f87e37070
Janik Chally <janikchally@gmail.com>
2022-01-16 16:05:11 -0600

    Started to do operations

--- SecondCalculator/scripts/script.js
+++ SecondCalculator/scripts/script.js
@@ -5,7 +5,9 @@ let buttons = document.querySelector('.calc-button');
 buttons.addEventListener('click', (event) => {
     const elmt = event.target;
     const display = document.querySelector('.calc-input');
-    console.log(elmt)
+    const calculator = document.querySelector('.calculator');
+    
+    calculator.dataset.firstNumber = '12';
 
     if (elmt.id === 'double') {
         document.querySelector('.calc-input').value *= 2;
@@ -13,7 +15,12 @@ buttons.addEventListener('click', (event) => {
     } else if (elmt.id === 'third') {
         document.querySelector('.calc-input').value *= .33;
 
-    } else if (elmt.className === 'number-button') {
+    }else if (elmt.id ==='decimal'){//if for decimals
+        if(!display.value.includes('.')){
+            display.value += elmt.textContent;
+
+        }
+    } else if (elmt.classList.contains('number-button')) {//if for numbers
         if(display.value === '0'){
             display.value = elmt.textContent;
         }else{
@@ -22,11 +29,7 @@ buttons.addEventListener('click', (event) => {
 
     }else if(elmt.id === 'clear'){
         display.value = 0;
-    }else if (elmt.id ==='decimal'){
-        if(!display.value.includes('.')){
-            display.value += elmt.textContent;
-        }
-    }else if(elmt.id ==='negative'){
+    }else if(elmt.id ==='negative'){//Button for +/- sign
         if(!display.value.includes('-')){
             if(display.value !=='0'){
                 display.value = '-' + display.value;
@@ -35,9 +38,31 @@ buttons.addEventListener('click', (event) => {
         else {
             display.value = display.value.slice(1);
         }
+    }else if(elmt.id ==='perc'){
+        display.value = display.value / 100;
     }
 
+    
+    else if(elmt.classList.contains('operator-button')){
+        calculator.dataset.firstNumber=display.value;//stores current value
+        calculator.dataset.secondElmt=elmt.id;//stores operand elemt pressed
+        console.log(calculator.dataset);
+        
+    }
 
+    else if(elmt.id === 'equal'){
+        if(calculator.dataset.firstNumber && calculator.dataset.secondElmt){
+            console.log('Passed');
 
+        }
+        else{
+            console.log('not passed');
+        }
+        calculator.dataset.firstnumber ="";
+        calculator.dataset.secondElmt ="";
+    }
+    let calculate = (firstNum,operand,secondNum)=> {
+        return firstNum `${operand}` secondNum;
+        } 
 })
 
